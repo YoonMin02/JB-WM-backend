62440e66264b05ebf85c43092b0eded646b93b06
YoonMin02 <tomasyms@gmail.com>
2026-06-12 02:17:30 +0900

    chores: allow local front access

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="JB WM Agent", version="0.1.0", lifespan=lifespan)
 # 프론트(개발 서버)에서 호출 허용
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://jb-wm.netlify.app"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "https://jb-wm.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
